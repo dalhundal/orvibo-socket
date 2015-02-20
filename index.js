9fd25aa129acfcfad8ade222965ea5f370bbba95
Dal Hundal <dal@dalhundal.com>
2015-02-20 00:41:44 +0000

    Control state of socket

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var OrviboSocket = require('./lib/orvibo-socket');
 
 OrviboSocket.discover();
+
+OrviboSocket.on('discovered',function(socket) {
+	socket.on = true;
+	setTimeout(function() {
+		socket.on = false;
+	},2000);
+});
