433c0e8e819beb85241804def41e406e5f7625d7
Dal Hundal <dal@dalhundal.com>
2015-02-20 11:09:17 +0000

    Pass on messages to socket

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var OrviboSocket = require('./lib/orvibo-socket');
 OrviboSocket.discover();
 
 OrviboSocket.on('discovered',function(socket) {
-	socket.on = true;
+	socket.state = true;
 	setTimeout(function() {
-		socket.on = false;
+		socket.state = false;
 	},2000);
+	//
+	socket.on('change',function() {
+		console.log("State change of socket",socket.name);
+	});
 });
