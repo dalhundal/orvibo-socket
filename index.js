f144057bd19daa16a84019793c538701e1e41450
Dal Hundal <dal@dalhundal.com>
2015-02-20 12:38:54 +0000

    Change method of setting swich state to a function

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ var OrviboSocket = require('./lib/orvibo-socket');
 OrviboSocket.discover();
 
 OrviboSocket.on('discovered',function(socket) {
-	socket.state = true;
-	setTimeout(function() {
-		socket.state = false;
-	},2000);
-	//
-	socket.on('change',function() {
-		console.log("State change of socket",socket.name);
+	console.log("Discovered socket %s", socket.toString());
+	
+	socket.on('change',function(ev) {
+		console.log("[%s] is %s", socket.name, ev ? 'ON' : 'OFF');
 	});
+
+	console.log("Switching on...");
+	socket.state(true);
+
+
+	setTimeout(function() {
+		console.log("Switching off..");
+		socket.state(false);
+	},2000);
+
 });
